159092063d1ff4a762cd480e77e38dacd0d0f5d8
Nivedhitha Venugopal <nivedhitha_venugopal@homedepot.com>
2017-05-02 15:12:59 -0400

    Adding multiple components

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,21 @@
-console.log("It works");
+import React from "react";
+import { render } from "react-dom";
+import  { Header } from "./components/Header";
+import { Home } from './components/Home';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<Header/>
+				</div>
+				<div className="row">
+					<Home/>
+				</div>
+			</div>
+		);
+	}
+}
+
+render(<App/>, window.document.getElementById("app"));
