fac5a3c6e64e2b03e18639b785fb17580db696ca
Nivedhitha Venugopal <nivedhitha_venugopal@homedepot.com>
2017-05-03 09:31:48 -0400

    Stateless components

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,9 @@
-import React from "react";
-import { render } from "react-dom";
-import  { Header } from "./components/Header";
+import React from 'react';
+import { render } from 'react-dom';
+import { Header } from './components/Header';
 import { Home } from './components/Home';
 import { Events } from './components/Events';
+import { StatelessHeader } from './components/StatelessHeader';
 
 class App extends React.Component {
 	render() {
@@ -15,6 +16,11 @@ class App extends React.Component {
 				<div className="row">
 					<div className="col-xs-10 col-xs-offset-1">
 						<Header/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-10 col-xs-offset-1">
+						<StatelessHeader homeLink="Home in Stateless Header"/>
 					</div>
 				</div>
 				<div className="row">
